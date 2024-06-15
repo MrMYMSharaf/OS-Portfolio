4eccd21473424cd5efd797e7b231778247a91ecd
Hi <mymsharaff@gmail.com>
2024-06-15 11:55:45 +0530

    cv-icone open

--- src/application/windowAplication/Cv.jsx
+++ src/application/windowAplication/Cv.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import assets from '../../assets';
 
-
 const Cv = ({ CvClose }) => {
-  
+  const pdfUrl = 'src/assets/cv_pdf/MYM_SHARAF_CV.pdf'; // Replace with the actual path to your PDF file
+
   return (
     <div>
       <assets.Layer icon={assets.Certificate} image={assets.Exit} name={'CV'} onClose={CvClose}>
-        <span>hi</span>
+        <iframe src={pdfUrl} width="100%" height="600px" title="CV PDF" />
       </assets.Layer>
     </div>
   );
--- src/component/Window.jsx
+++ src/component/Window.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
-import { motion } from 'framer-motion'; // Import motion from framer-motion
+import React, { useEffect, useState, useLayoutEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import assets from '../assets';
 import ContextMenu from '../application/ContextMenu';
-import Calculator  from '../application/Calculator';
-import Certificate from '../application/windowAplication/Certificate';
 
-// DraggableWindow component
-const DraggableWindow = ({ dragConstraints, windowPosition, children,className}) => {
+const DraggableWindow = ({ dragConstraints, windowPosition, children, className }) => {
   return (
     <motion.div
-      // className="w-20 h-24 cursor-grab"
       className={className}
       drag
       dragMomentum={false}
       dragConstraints={dragConstraints}
       dragElastic={0}
       dragTransition={{ bounceDamping: 10 }}
-      style={{x: windowPosition.x, y: windowPosition.y, position: 'absolute', zIndex: 1 }}
+      style={{ x: windowPosition.x, y: windowPosition.y, position: 'absolute', zIndex: 1 }}
     >
       {children}
     </motion.div>
@@ -26,47 +22,66 @@ const DraggableWindow = ({ dragConstraints, windowPosition, children,className})
 const Window = (props) => {
   const [dragConstraints, setDragConstraints] = useState({ left: 0, right: 0, top: 0, bottom: 0 });
   const [windowPosition, setWindowPosition] = useState({ x: 0, y: 0 });
-  const [showContextMenu, setShowContextMenu] = useState(false); // State to control the visibility of the context menu
-  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 }); // Position of the context menu
-  const { menuVisible, toggleMenu} = props;
+  const [showContextMenu, setShowContextMenu] = useState(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
+  const { menuVisible, toggleMenu } = props;
 
-  const [clickCount, setClickCount] = useState(0);
+  const [clickCount, setClickCount] = useState({ 0: 0, 1: 0, 2: 0, 3: 0 });
   const [showCertificate, setShowCertificate] = useState(false);
-  const [certificateIconBackgroundColor, setCertificateIconBackgroundColor] = useState("");
+  const [showCv, setShowCv] = useState(false);
+  const [showBlog, setShowBlog] = useState(false);
+  const [iconBackgroundColor, setIconBackgroundColor] = useState({
+    0: "",
+    1: "",
+    2: "",
+    3: ""
+  });
 
+  const iconRefs = useRef([]);
+  const windowRef = useRef(null);
 
   const iconPositions = [
     { x: 0, y: 10 },
-    { x: 0, y: 90 }, // Adjust x and y values for each icon
+    { x: 0, y: 90 },
     { x: 0, y: 170 },
     { x: 0, y: 250 }
-    // Add more positions as needed
   ];
-  
 
   const handleDragStart = (event) => {
     event.preventDefault();
   };
 
-  // Handle right-click event
-const handleRightClick = (event) => {
-  event.preventDefault();
-  //console.log("Mouse coordinates:", event.clientX, event.clientY);
-  setContextMenuPosition({ x: event.clientX, y: event.clientY }); // Set the position of the context menu
-  setShowContextMenu(true); // Show the context menu
-};
+  const handleRightClick = (event) => {
+    event.preventDefault();
+    setContextMenuPosition({ x: event.clientX, y: event.clientY });
+    setShowContextMenu(true);
+  };
 
-
-
-  // Close context menu when clicking outside
   const handleClickOutsideContextMenu = () => {
     setShowContextMenu(false);
   };
 
   useEffect(() => {
+    const handleClickOutside = (event) => {
+      let clickedOutside = true;
+
+      iconRefs.current.forEach((icon, index) => {
+        if (icon && icon.contains(event.target)) {
+          clickedOutside = false;
+        }
+      });
+
+      if (clickedOutside) {
+        setClickCount({ 0: 0, 1: 0, 2: 0, 3: 0 });
+        setIconBackgroundColor({ 0: "", 1: "", 2: "", 3: "" });
+      }
+    };
+
+    window.addEventListener('click', handleClickOutside);
     window.addEventListener('click', handleClickOutsideContextMenu);
 
     return () => {
+      window.removeEventListener('click', handleClickOutside);
       window.removeEventListener('click', handleClickOutsideContextMenu);
     };
   }, []);
@@ -74,8 +89,8 @@ const handleRightClick = (event) => {
   useLayoutEffect(() => {
     function updateWindowDimensions() {
       const { innerWidth, innerHeight } = window;
-      const windowWidth = 10; // Width of the window
-      const windowHeight = 100; // Height of the window
+      const windowWidth = 10;
+      const windowHeight = 100;
 
       setDragConstraints({
         left: 0,
@@ -85,7 +100,6 @@ const handleRightClick = (event) => {
       });
     }
 
-    // Update dimensions when component mounts or when window is resized
     updateWindowDimensions();
     setWindowPosition({ x: 0, y: 0 });
 
@@ -100,73 +114,107 @@ const handleRightClick = (event) => {
 
   const clickwindow = () => {
     if (menuVisible) {
-      toggleMenu(); // Close the menu only if it's open
+      toggleMenu();
     }
   };
 
-  const handleClickCertificate = () => {
-    setClickCount(prevCount => {
-      const newCount = prevCount + 1;
-      
-      if (newCount === 1) {
-        setCertificateIconBackgroundColor("bg-orange-500 bg-opacity-25 w-full h-full");
-      } else if (newCount === 2) {
-        setCertificateIconBackgroundColor("");
-        setShowCertificate(true);
+  const handleClickIcon = (index) => {
+    setClickCount(prevCounts => {
+      const newCount = prevCounts[index] + 1;
+
+      setIconBackgroundColor(prevColors => ({
+        ...prevColors,
+        [index]: newCount === 1 ? "bg-orange-500 bg-opacity-25 w-full h-full" : ""
+      }));
+
+      if (newCount === 2) {
+        if (index === 0) {
+          // Handle My Computer
+        } else if (index === 1) {
+          setShowCv(true);
+        } else if (index === 2) {
+          setShowBlog(true);
+        } else if (index === 3) {
+          setShowCertificate(true);
+        }
       }
 
-      return newCount === 2 ? 0 : newCount;
+      return {
+        ...prevCounts,
+        [index]: newCount === 2 ? 0 : newCount
+      };
     });
   };
+
+  const handleCloseCv = () => {
+    setShowCv(false);
+  };
+
+  const handleCloseBlog = () => {
+    setShowBlog(false);
+  };
+
   const handleCloseCertificate = () => {
     setShowCertificate(false);
   };
-  
 
-  
-
-  
- return (
-    <div className="relative h-screen bg-gray-200 overflow-hidden overflow-x-hidden overflow-y-clip" style={{backgroundImage: `url(${assets.bg0})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%'}} onClick={clickwindow} onContextMenu={handleRightClick}>
-      {/* DraggableWindows for icons */}
+  return (
+    <div ref={windowRef} className="relative h-screen bg-gray-200 overflow-hidden overflow-x-hidden overflow-y-clip" style={{ backgroundImage: `url(${assets.bg0})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%' }} onClick={clickwindow} onContextMenu={handleRightClick}>
       {iconPositions.map((position, index) => (
         <DraggableWindow key={index} dragConstraints={dragConstraints} windowPosition={position} className="w-20 h-24 cursor-grab">
-          {index === 0 && (
-            <>
-              <img src={assets.mycomputer} alt="mycomputer" onDragStart={handleDragStart} className='w-12 h-12 relative' />
-              <span className='text-white text-xs ml-1 absolute'>My Computer</span>
-            </>
-          )}
-          {index === 1 && (
-            <>
-              <img src={assets.RTF} alt="mycomputer" onDragStart={handleDragStart} className='w-12 h-12 relative' />
-              <span className='text-white text-xs ml-1 absolute'>My CV</span>
-            </>
-          )}
-          {index === 2 && (
-            <>
-              <img src={assets.Briefcase} alt="mycomputer" onDragStart={handleDragStart} className='w-12 h-12 relative' />
-              <span className='text-white text-xs ml-1 absolute'>My Blog</span>
-            </>
-          )}
-          {index === 3 && (
-            <div onClick={handleClickCertificate} className={certificateIconBackgroundColor}>
-              <img src={assets.Certificate} alt="mycomputer" onDragStart={handleDragStart} className='w-12 h-12 relative' />
-              <span className='text-white text-xs ml-1 absolute'>Certificate</span>
-            </div>
-          )}
+          <div ref={el => (iconRefs.current[index] = el)} onClick={() => handleClickIcon(index)} className={iconBackgroundColor[index]}>
+            {index === 0 && (
+              <>
+                <img src={assets.mycomputer} alt="mycomputer" onDragStart={handleDragStart} className='w-12 h-12 relative' />
+                <span className='text-white text-xs ml-1 absolute'>My Computer</span>
+              </>
+            )}
+            {index === 1 && (
+              <>
+                <img src={assets.RTF} alt="mycv" onDragStart={handleDragStart} className='w-12 h-12 relative' />
+                <span className='text-white text-xs ml-1 absolute'>My CV</span>
+              </>
+            )}
+            {index === 2 && (
+              <>
+                <img src={assets.Briefcase} alt="myblog" onDragStart={handleDragStart} className='w-12 h-12 relative' />
+                <span className='text-white text-xs ml-1 absolute'>My Blog</span>
+              </>
+            )}
+            {index === 3 && (
+              <>
+                <img src={assets.Certificate} alt="certificate" onDragStart={handleDragStart} className='w-12 h-12 relative' />
+                <span className='text-white text-xs ml-1 absolute'>Certificate</span>
+              </>
+            )}
+          </div>
         </DraggableWindow>
       ))}
 
-      {/* Render Certificate component outside of DraggableWindow */}
-      <DraggableWindow dragConstraints={dragConstraints} windowPosition={{ x: 200, y: 30 }} className="w-1/2 h-20 cursor-grab">
-        {showCertificate && (
+      {showCv && (
+        <DraggableWindow dragConstraints={dragConstraints} windowPosition={{ x: 200, y: 30 }} className="w-1/2 h-20 cursor-grab">
           <div>
-            <Certificate CertificateonClose={handleCloseCertificate}/>
+            <assets.Cv CvClose={handleCloseCv} />
           </div>
-        )}
-      </DraggableWindow>
-      {/* Render ContextMenu */}
+        </DraggableWindow>
+      )}
+
+      {showBlog && (
+        <DraggableWindow dragConstraints={dragConstraints} windowPosition={{ x: 200, y: 30 }} className="w-1/2 h-20 cursor-grab">
+          <div>
+            <assets.Blog BlogClose={handleCloseBlog} />
+          </div>
+        </DraggableWindow>
+      )}
+
+      {showCertificate && (
+        <DraggableWindow dragConstraints={dragConstraints} windowPosition={{ x: 200, y: 30 }} className="w-1/2 h-20 cursor-grab">
+          <div>
+            <assets.Certificates CertificateonClose={handleCloseCertificate} />
+          </div>
+        </DraggableWindow>
+      )}
+
       {showContextMenu && <ContextMenu top={contextMenuPosition.y} left={contextMenuPosition.x} />}
     </div>
   );
